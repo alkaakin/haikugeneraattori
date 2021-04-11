faea43881c3b4fba59227786722811a172d8a4b3
Aleksi <aleksi.kaakinen@helsinki.fi>
2021-04-11 13:15:53 +0300

    Added SQL Schema, deleted one index template

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE haikut (
+    id INTEGER,
+    nimi TEXT,
+    arvosana INTEGER,
+    genre TEXT,
+    visible INTEGER,
+    content TECT
+);
